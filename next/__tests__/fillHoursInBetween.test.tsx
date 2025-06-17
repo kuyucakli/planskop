2dcee2ac49f3dedffbffa569730407b9a61029ee
Burak Kuyucaklı <burak.kuyucakli@gmail.com>
2025-06-17 17:58:47 +0300

    Add some tests

---
+++ next/__tests__/fillHoursInBetween.test.tsx
@@ -0,0 +1,18 @@
+import { fillHoursInbetween } from "@/components/HourlyRoutinesChart";
+
+
+describe("fill hours", () => {
+    test("start hour < end hour", () => {
+        expect(fillHoursInbetween("21:00", "23:00")).toEqual([21, 22, 23]);
+    })
+
+
+    test("start hour > end hour ", () => {
+        expect(fillHoursInbetween("21:00", "00:00")).toEqual([21, 22, 23, 0]);
+    })
+
+    test("hour 00:00", () => {
+        expect(fillHoursInbetween("00:00", "03:00")).toEqual([0, 1, 2, 3]);
+    })
+
+})
