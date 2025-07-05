143a1244fdd88d97084bc4cac5671307cc6a090a
Burak Kuyucaklı <burak.kuyucakli@gmail.com>
2025-07-05 12:10:35 +0300

    Refine for mobile screens

---
+++ next/components/MainNavBar.tsx
@@ -0,0 +1,20 @@
+
+
+import { NavMain } from "./navs/NavMain";
+
+
+const MainNavBar = async () => {
+
+    return (
+        <div className="main-nav-bar py-8 md:py-6 bg-bermuda md:bg-gray-700">
+
+            <NavMain />
+
+
+        </div>
+    )
+}
+
+
+
+export default MainNavBar;
