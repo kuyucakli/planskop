2438f688fb93ec10c9d78f21ca308a1747d5c433
Burak Kuyucaklı <burak.kuyucakli@gmail.com>
2025-07-05 12:13:20 +0300

    Refine for mobile screens

--- next/components/MainNavBar.tsx
+++ next/components/MainNavBar.tsx
@@ -6,7 +6,7 @@ import { NavMain } from "./navs/NavMain";
 const MainNavBar = async () => {
 
     return (
-        <div className="main-nav-bar py-8 md:py-6 bg-bermuda md:bg-gray-700">
+        <div className="main-nav-bar py-8 md:py-6 bg-bermuda md:bg-gray-700 z-50">
 
             <NavMain />
 
