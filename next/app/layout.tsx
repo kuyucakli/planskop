096c6223ca76f5929f1d84b9e7f92d11e1d9f8d8
Burak Kuyucaklı <burakkuyucakli@Buraks-MacBook-Air.local>
2025-08-17 13:21:30 +0300

    Refine small screen navbar bg

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -1,4 +1,4 @@
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
 import { CardFamousPersonSummary } from "@/components/Card";
 import { FamousPeopleRoutines } from "./famous-people-routines";
 import MainNavBar from "@/components/MainNavBar";
@@ -10,63 +10,69 @@ import { Roboto_Flex } from "next/font/google";
 import { Suspense } from "react";
 import { ThemeContextProvider } from "@/context/ThemeContext";
 import "./globals.css";
-import AppIntro from '@/components/AppIntro';
+import AppIntro from "@/components/AppIntro";
 import appIntroStyles from "@/components/AppIntro.module.css";
 
-const robotoFlex = Roboto_Flex({ subsets: ["latin"], display: "swap", variable: '--font-roboto-flex', });
-const kiraHareng = Kirang_Haerang({ weight: "400", display: "swap", variable: '--font-kira-hareng', });
+const robotoFlex = Roboto_Flex({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto-flex",
+});
+const kiraHareng = Kirang_Haerang({
+  weight: "400",
+  display: "swap",
+  variable: "--font-kira-hareng",
+});
 
 export const metadata: Metadata = {
   title: "Planskop",
-  description: "Welcome to Planskop – Your Personal Guide to Building Better Habits",
+  description:
+    "Welcome to Planskop – Your Personal Guide to Building Better Habits",
 };
 
 export default async function RootLayout({
   children,
-
-
 }: Readonly<{
   children: React.ReactNode;
-
 }>) {
-
-
   return (
-
     <ClerkProvider>
-      <ThemeContextProvider>
-
-      </ThemeContextProvider>
-      <html lang="en" >
+      <ThemeContextProvider></ThemeContextProvider>
+      <html lang="en">
         <head>
           <link rel="icon" href="/favicon.ico" />
         </head>
-        <body className={`font-mono ${kiraHareng.variable} ${appIntroStyles.AppIntroBody}`}>
-          <AppIntro/>
+        <body
+          className={`font-mono ${kiraHareng.variable} ${appIntroStyles.AppIntroBody}`}
+        >
+          <AppIntro />
           <div id="root" className="md:overflow-hidden">
             <MainNavBar />
             <header className="p-2 ">
-              
               <NavUser />
-             
             </header>
             <main className="p-3 md:p-6 limited-width overflow-y-auto bg-linear-to-b from-zinc-600 via-stone-800 to-zinc-900">
-
               {children}
-
             </main>
 
             <aside className="p-6 overflow-y-auto bg-linear-to-b from-zinc-600 via-stone-800 to-zinc-900">
-              <h1 className="text-4xl/16  l-h sticky top-0 left-0 backdrop-blur-sm">Inspiration</h1>
-              <Suspense fallback={<CardFamousPersonSummary famousPerson={{ image: "", personName: "", routines: [] }} isSkeletonView />}>
+              <h1 className="text-4xl/16  l-h sticky top-0 left-0 backdrop-blur-sm">
+                Inspiration
+              </h1>
+              <Suspense
+                fallback={
+                  <CardFamousPersonSummary
+                    famousPerson={{ image: "", personName: "", routines: [] }}
+                    isSkeletonView
+                  />
+                }
+              >
                 <FamousPeopleRoutines />
               </Suspense>
             </aside>
-
           </div>
         </body>
       </html>
     </ClerkProvider>
-
   );
 }
--- next/components/MainNavBar.tsx
+++ next/components/MainNavBar.tsx
@@ -1,20 +1,11 @@
-
-
 import { NavMain } from "./navs/NavMain";
 
-
 const MainNavBar = async () => {
-
-    return (
-        <div className="main-nav-bar py-0 md:py-6 md:bg-black  bg-linear-to-b from-zinc-600 via-stone-800 to-zinc-900 z-50">
-
-            <NavMain />
-
-
-        </div>
-    )
-}
-
-
+  return (
+    <div className="main-nav-bar py-0 md:py-6 bg-zinc-900 md:bg-linear-to-b from-zinc-600 via-stone-800 to-zinc-900 z-50">
+      <NavMain />
+    </div>
+  );
+};
 
 export default MainNavBar;
